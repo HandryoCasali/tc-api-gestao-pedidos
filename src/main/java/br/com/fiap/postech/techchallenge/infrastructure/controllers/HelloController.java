143d18902ac6417a9beeb36d350dae8478ff89b8
Handryo Casali <handryo@hotmail.com>
2024-12-07 20:59:43 -0300

    Adicionando controller test pipeline

---
+++ src/main/java/br/com/fiap/postech/techchallenge/infrastructure/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package br.com.fiap.postech.techchallenge.infrastructure.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    
+    @GetMapping("")
+    public String hello() {
+        return new String("Hello");
+    }
+    
+}
